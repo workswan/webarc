d0ae29b802bc3b261266c9563f1a50a0a9ccabda
works <workswan@163.com>
2018-12-04 21:34:54 +0800

    webarc

--- src/main/java/org/smart4j/chapter1/HelloServletCopy.java
+++ src/main/java/org/smart4j/chapter1/HelloServletCopy.java
@@ -11,7 +11,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @WebServlet("/hello1")
-public class HelloServlet1 extends HttpServlet{
+public class HelloServletCopy extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
